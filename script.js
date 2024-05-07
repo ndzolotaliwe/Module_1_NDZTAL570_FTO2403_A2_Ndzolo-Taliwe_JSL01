517e6c2de3d0e64aa14cc1eb6bab7bdcb7647bea
Ndzolo Taliwe <ndztaliwe@gmail.com>
2024-05-07 19:25:05 +0300

    commit changes

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function validateSyntax() {
     let result = ''; 
     let regex = /^pet_[a-zA-Z0-9]+$/; 
     if (regex.test(input)) {
-        result = 'Valid input'; 
+        result = 'Valid Syntax \u{1F7E2}'; 
     } else {
-        result = 'Invalid input'; 
+        result = 'Invalid Syntax \u{1F534} ';  
     }
 
             document.getElementById('result').innerText = result;
